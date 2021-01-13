aa46170e420f420ee229c3227bc528d5403ca75f
goess233 <h1hzz@hotmail.com>
2021-01-14 01:08:23 +0800

    try turtle

--- mktdt.py
+++ mktdt.py
@@ -1,39 +1,55 @@
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-# import baostock as bs
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
 import akshare as ak
 import datetime
 import time
 from itertools import product
 
 # shfe_text = ak.match_main_contract(exchange="shfe")
-from pandas import DataFrame
-
+TRADE_IF_LAST_WIN = True
+ENTRY_FAILSAFE_BREAKOUT = 55
+ENTRY_BREAKOUT = 20
+ENTRY_OFFSET_ATR = 0
+ADD_ATR = 0.5
+STOP_ATR = 2
+EXIT_BREAKOUT = 10
+EXIT_OFFSET_ATR = 0
+MAX_UNITS = 4
+ATR_WIN = 20
 
 # 海龟交易法，顺势交易，逆鞅方法的应用
 def turtle():
-    ag_df = ak.futures_zh_minute_sina(symbol="AG0",period='30')
+    # ag_df = ak.futures_zh_minute_sina(symbol="AG0",period='60')  # 37 frames a day if period equals 15
     # end_date = datetime.datetime.today().strftime('%Y%m%d')
     # ag_df = ak.get_futures_daily(start_date='20190101',end_date=end_date,)
+    ag_df = ak.futures_foreign_hist(symbol="XAG")
+    ag_df = ag_df.iloc[-365:]
     ag_df[['open','high','low','close']] = ag_df[['open','high','low','close']].astype(float)
     atr(ag_df)
     donchian(ag_df)
+    parameters = {}
+    parameters['long_add']= ag_df['entry_high']+ag_df['ATR']*ADD_ATR
+    print(parameters)
     return ag_df
 
 
 def atr(df):
-    df['tr1'] = df['high']-df['low']
-    df['tr2'] = df['high']-df['close'].shift(1)
-    df['tr3'] = df['low']-df['close'].shift(1)
-    df['TR'] = df[['tr1', 'tr2', 'tr3']].max(axis=1)
-    df['ATR'] = df['TR'].rolling(window=20).mean()
+    temp = pd.DataFrame()
+    temp['tr1'] = df['high']-df['low']
+    temp['tr2'] = df['high']-df['close'].shift(1)
+    temp['tr3'] = df['low']-df['close'].shift(1)
+    df['TR'] = temp[['tr1', 'tr2', 'tr3']].max(axis=1)
+    df['ATR'] = df['TR'].rolling(window=ATR_WIN).mean()
 
 def donchian(df):
-    df['d_high'] = df['high'].rolling(window=20).max()
-    df['d_low'] = df['low'].rolling(window=20).min()
-
+    df['entry_high'] = df['high'].rolling(window=ENTRY_BREAKOUT).max()
+    df['entry_low'] = df['low'].rolling(window=ENTRY_BREAKOUT).min()
+    df['entry_mid'] = (df['entry_high']+df['entry_low'])/2
+    df['exit_high'] = df['high'].rolling(window=EXIT_BREAKOUT).max()
+    df['exit_low'] = df['low'].rolling(window=EXIT_BREAKOUT).min()
+    # long_open,short_open
 
 
 # 网格，左侧交易，适用于震荡行情，或者波动较大，同时又有上下界的品种，没有止损
@@ -124,27 +140,30 @@ def spider(future):
 
 
 if __name__ == "__main__":
-    rank = spider(['AG'])
-    print("------------------------------------------------")
-    aurank = time.time_ns()
-    raw_index_df = ak.stock_zh_index_daily(symbol="sz399300")
-    sma1 = range(20, 61, 4)
-    sma2 = range(180, 281, 10)
-    results = pd.DataFrame()
-    for SMA1, SMA2 in product(sma1, sma2):
-        data: DataFrame = pd.DataFrame(raw_index_df['close'])
-        data.dropna(inplace=True)
-        data['returns'] = np.log(data['close'] / data['close'].shift(1))
-        data['sma1'] = data['close'].rolling(SMA1).mean()
-        data['sma2'] = data['close'].rolling(SMA2).mean()
-        data.dropna(inplace=True)
-        data['position'] = np.where(data['sma1'] > data['sma2'], 1, 0)
-        data['strategy'] = data['position'].shift(1) * data['returns']
-        data.dropna(inplace=True)
-        perf = np.exp(data[['returns', 'strategy']].sum())
-        results = results.append(
-            pd.DataFrame({'SMA1': SMA1, 'SMA2': SMA2, 'market': perf['returns'], 'strategy': perf['strategy']},
-                         index=[0]), ignore_index=True)
-    print(results.info())
-    print(results.sort_values('strategy', ascending=False))
-    results.shift()
+    # rank = spider(['AG'])
+    # print("------------------------------------------------")
+    # aurank = time.time_ns()
+    # raw_index_df = ak.stock_zh_index_daily(symbol="sz399300")
+    # sma1 = range(20, 61, 4)
+    # sma2 = range(180, 281, 10)
+    # results = pd.DataFrame()
+    # for SMA1, SMA2 in product(sma1, sma2):
+    #     data: DataFrame = pd.DataFrame(raw_index_df['close'])
+    #     data.dropna(inplace=True)
+    #     data['returns'] = np.log(data['close'] / data['close'].shift(1))
+    #     data['sma1'] = data['close'].rolling(SMA1).mean()
+    #     data['sma2'] = data['close'].rolling(SMA2).mean()
+    #     data.dropna(inplace=True)
+    #     data['position'] = np.where(data['sma1'] > data['sma2'], 1, 0)
+    #     data['strategy'] = data['position'].shift(1) * data['returns']
+    #     data.dropna(inplace=True)
+    #     perf = np.exp(data[['returns', 'strategy']].sum())
+    #     results = results.append(
+    #         pd.DataFrame({'SMA1': SMA1, 'SMA2': SMA2, 'market': perf['returns'], 'strategy': perf['strategy']},
+    #                      index=[0]), ignore_index=True)
+    # print(results.info())
+    # print(results.sort_values('strategy', ascending=False))
+    # results.shift()
+    df = turtle()
+    print(df.iloc[-1])
+
